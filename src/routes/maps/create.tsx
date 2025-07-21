149a4664a877ea659bd78d40061788f4600d2396
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-07-21 17:09:17 -0400

    Add tooltips to control buttons

--- src/routes/maps/create.tsx
+++ src/routes/maps/create.tsx
@@ -1,4 +1,9 @@
 import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import { createFileRoute } from "@tanstack/react-router";
 import { Hand, MousePointer, Play, Settings } from "lucide-react";
@@ -8,15 +13,26 @@ export const Route = createFileRoute("/maps/create")({
   component: RouteComponent,
 });
 
-const ControlBarButton = ({ ...props }: HTMLAttributes<HTMLElement>) => {
-  const { children, className, ...rest } = props;
+const ControlBarButton = ({
+  ...props
+}: HTMLAttributes<HTMLElement> & { toolTip?: string }) => {
+  const { children, className, toolTip, ...rest } = props;
   return (
-    <Button
-      {...rest}
-      className={cn("bg-transparent hover:bg-white/20", className)}
-    >
-      {children}
-    </Button>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button
+          {...rest}
+          className={cn("bg-transparent hover:bg-white/20", className)}
+        >
+          {children}
+        </Button>
+      </TooltipTrigger>
+      {toolTip && (
+        <TooltipContent>
+          <p>{toolTip}</p>
+        </TooltipContent>
+      )}
+    </Tooltip>
   );
 };
 
@@ -26,16 +42,16 @@ function RouteComponent() {
       <div className="flex w-full justify-center p-4">
         <section className="flex max-w-2xl justify-end gap-0.5 rounded-md bg-neutral-700 p-1">
           <h1 className="sr-only">Map Generator Toolbar</h1>
-          <ControlBarButton>
+          <ControlBarButton toolTip="Pan">
             <Hand />
           </ControlBarButton>
-          <ControlBarButton>
+          <ControlBarButton toolTip="Select">
             <MousePointer />
           </ControlBarButton>
-          <ControlBarButton>
+          <ControlBarButton toolTip="Settings">
             <Settings />
           </ControlBarButton>
-          <ControlBarButton>
+          <ControlBarButton toolTip="Generate">
             <Play />
           </ControlBarButton>
         </section>
