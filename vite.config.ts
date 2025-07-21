720cd9ba65b054327fb2e481cbe2155e2ba13e12
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-07-20 20:01:05 -0400

    Config and init shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
       autoCodeSplitting: true,
     }) as PluginOption, // No idea why it doesn't match the type. It works so ???
     react(),
-    tailwindcss(),
+    tailwindcss() as PluginOption,
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  }
 });
