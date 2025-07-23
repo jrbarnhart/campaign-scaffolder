11bcaf651b16bbb1eaa83ef22f484e05063857f0
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-07-23 18:44:59 -0400

    Add html button attributes to props

--- src/components/controls/ControlBarButton.tsx
+++ src/components/controls/ControlBarButton.tsx
@@ -1,17 +1,21 @@
+import type { HTMLAttributes } from "react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 export default function ControlBarButton({
-  toolTip,
-  children,
-}: {
-  toolTip?: string;
-  children: React.ReactNode;
-}) {
+  ...props
+}: HTMLAttributes<HTMLButtonElement> & { toolTip?: string }) {
+  const { children, className, toolTip, ...rest } = props;
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <Button className="bg-transparent hover:bg-white/20">{children}</Button>
+        <Button
+          {...rest}
+          className={cn(className, "bg-transparent hover:bg-white/20")}
+        >
+          {children}
+        </Button>
       </TooltipTrigger>
       {toolTip && (
         <TooltipContent>
