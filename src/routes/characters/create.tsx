1704589a8b8e6577a9347d5e93a78189087e42b9
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-07-27 03:36:07 -0400

    Add menu and undo controls

--- src/routes/characters/create.tsx
+++ src/routes/characters/create.tsx
@@ -1,9 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router'
+import MainMenu from "@/components/controls/MainMenu";
+import UndoControls from "@/components/controls/UndoControls";
+import {
+  DropdownMenuItem,
+  DropdownMenuShortcut,
+} from "@/components/ui/dropdown-menu";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/characters/create')({
+export const Route = createFileRoute("/characters/create")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/characters/create"!</div>
+  return (
+    <div className="relative h-svh">
+      <div className="absolute right-4 bottom-4">
+        <UndoControls />
+      </div>
+      <div className="absolute bottom-4 left-4 md:top-4">
+        <MainMenu>
+          <DropdownMenuItem>
+            Create New Character <DropdownMenuShortcut>⌘N</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </MainMenu>
+      </div>
+    </div>
+  );
 }
