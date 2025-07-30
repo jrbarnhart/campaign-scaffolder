710d713942b3411f0a7a8d9a2a64712304790a9b
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-07-30 00:37:48 -0400

    Add header and use form component

--- src/routes/characters/create.tsx
+++ src/routes/characters/create.tsx
@@ -1,10 +1,10 @@
 import MainMenu from "@/components/controls/MainMenu";
-import UndoControls from "@/components/controls/UndoControls";
+import CreateNpcForm from "@/components/forms/characters/CreateNpcForm";
+
 import {
   DropdownMenuItem,
   DropdownMenuShortcut,
 } from "@/components/ui/dropdown-menu";
-
 import generateNpc from "@/lib/characters/generateNpc";
 import type { NPC } from "@/lib/zodSchemas/npcSchema";
 import { createFileRoute } from "@tanstack/react-router";
@@ -24,41 +24,9 @@ function RouteComponent() {
 
   return (
     <div className="relative h-svh overflow-hidden">
-      <div className="absolute right-4 bottom-4">
-        <UndoControls />
-      </div>
-      <section className="overflow-auto p-2">
-        {/* <form className="bg-pink-300">
- 
-            <p>Name: {npc.name}</p>
- 
-            <p>Race: {npc.race}</p>
- 
-            <p>Sex: {npc.sex}</p>
- 
-            <p>
-              Pronouns:{" "}
-              {`${npc.pronouns[0][0].toUpperCase() + npc.pronouns[0].slice(1)}/${npc.pronouns[1][0].toUpperCase() + npc.pronouns[1].slice(1)} `}
-            </p>
- 
-            <div>
-              {npc.abilities.map((ability, index) => (
-                <p key={`${ability.name}-${index.toString()}`}>
-                  {ability.name}: {ability.value}
-                </p>
-              ))}
-            </div>
- 
-            <p>Alignment: {npc.alignment}</p>
- 
-            <div>
-              {npc.personality.map((trait, index) => (
-                <p key={`trait-${index.toString()}`}>{trait}</p>
-              ))}
-            </div>
- 
-            <p>Quirk: {npc.quirk}</p>
-          </form> */}
+      <section className="space-y-4 overflow-auto p-2">
+        <h1>Create a New NPC</h1>
+        <CreateNpcForm />
       </section>
       <div className="absolute bottom-4 left-4 md:top-4">
         <MainMenu>
