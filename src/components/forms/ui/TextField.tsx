4a0a4d8817f3565add7f41d550bfcc3c68870824
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-07-29 23:15:53 -0400

    Add data list functionality

--- src/components/forms/ui/TextField.tsx
+++ src/components/forms/ui/TextField.tsx
@@ -2,12 +2,13 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useFieldContext } from "@/contexts/reactFormContexts";
 
-export function TextField({ label }: { label: string }) {
+export function TextField({ label, list }: { label: string; list?: string }) {
   const field = useFieldContext<string>();
   return (
     <Label className="flex-col items-start gap-1">
       <div>{label}</div>
       <Input
+        list={list}
         value={field.state.value}
         onChange={(e) => {
           field.handleChange(e.target.value);
