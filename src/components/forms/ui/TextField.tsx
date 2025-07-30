7ea5545fae57acb680ba799069770d70dd515a54
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-07-29 22:09:29 -0400

    Adjust flex layout

--- src/components/forms/ui/TextField.tsx
+++ src/components/forms/ui/TextField.tsx
@@ -5,7 +5,7 @@ import { useFieldContext } from "@/contexts/reactFormContexts";
 export function TextField({ label }: { label: string }) {
   const field = useFieldContext<string>();
   return (
-    <Label>
+    <Label className="flex-col items-start gap-1">
       <div>{label}</div>
       <Input
         value={field.state.value}
