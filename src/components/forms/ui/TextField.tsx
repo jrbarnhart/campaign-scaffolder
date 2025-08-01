cdb9f9075a5f3cd320e9eec96cdf09ae7734e616
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-08-01 18:43:38 -0400

    Add on blur and readjust structure

--- src/components/forms/ui/TextField.tsx
+++ src/components/forms/ui/TextField.tsx
@@ -1,19 +1,30 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useFieldContext } from "@/contexts/reactFormContexts";
+import { cn } from "@/lib/utils";
 
 export function TextField({ label, list }: { label: string; list?: string }) {
   const field = useFieldContext<string>();
   return (
-    <Label className="flex-col items-start gap-1">
-      <div>{label}</div>
+    <div>
+      <Label
+        className={cn(
+          !field.state.meta.isValid && "border-destructive",
+          "flex-col items-start gap-1",
+        )}
+        htmlFor={`field-${field.name}`}
+      >
+        {label}
+      </Label>
       <Input
+        id={`field-${field.name}`}
         list={list}
         value={field.state.value}
         onChange={(e) => {
           field.handleChange(e.target.value);
         }}
+        onBlur={field.handleBlur}
       />
-    </Label>
+    </div>
   );
 }
