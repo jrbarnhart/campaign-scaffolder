787373bdf95b351a64dd42fb8aa39f3a72d8c9c2
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-08-01 22:35:11 -0400

    Add comments and logs for addign in subfield errors

--- src/components/forms/ui/TextArrayField.tsx
+++ src/components/forms/ui/TextArrayField.tsx
@@ -1,4 +1,3 @@
-// TextArrayField.tsx
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useFieldContext } from "@/contexts/reactFormContexts";
@@ -8,31 +7,45 @@ export function TextArrayField({ label }: { label: string }) {
 
   return (
     <div>
-      <label>{label}</label>
-      {field.state.value.map((_, index) => {
+      <label>{label}s</label>
+      {field.state.value.length === 0 && <p>No {label}s exist yet.</p>}
+      {field.state.value.map((_, valueIndex) => {
         return (
-          <div key={`item-${index.toString()}`} className="mb-2 flex gap-2">
-            <Input
-              value={field.state.value[index] || ""}
-              onChange={(e) => {
-                const newArray = [...field.state.value];
-                newArray[index] = e.target.value;
-                field.handleChange(newArray);
-              }}
-            />
-            <Button
-              onClick={() => {
-                const newArray = field.state.value.filter(
-                  (_, index) => index !== index,
-                );
-                field.handleChange(newArray);
-              }}
-              type="button"
-              variant="destructive"
-              size="sm"
-            >
-              Remove
-            </Button>
+          <div key={`item-${valueIndex.toString()}`}>
+            <div className="mb-2 flex gap-2">
+              <Input
+                value={field.state.value[valueIndex] || ""}
+                onChange={(e) => {
+                  const newArray = [...field.state.value];
+                  newArray[valueIndex] = e.target.value;
+                  field.handleChange(newArray);
+                  console.log(field.state.meta.errors);
+                }}
+                onBlur={() => {
+                  field.handleBlur();
+                  console.log(field.state.meta.errors);
+                }}
+              />
+              <Button
+                onClick={() => {
+                  const newArray = field.state.value.filter(
+                    (_, i) => valueIndex !== i,
+                  );
+                  field.handleChange(newArray);
+                }}
+                type="button"
+                variant="destructive"
+                size="sm"
+              >
+                Remove
+              </Button>
+            </div>
+            {/* If there is an error in errors at the same index as the value index then display it */}
+            {field.state.meta.errors[valueIndex] !== undefined && (
+              <div className="text-destructive">
+                {/* Render error here. Might need to use subField approach? https://tanstack.com/form/v1/docs/framework/react/guides/form-composition */}
+              </div>
+            )}
           </div>
         );
       })}
