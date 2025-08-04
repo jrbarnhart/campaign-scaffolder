45d9695fdd677525e1fa7bfab295650abbc0e8c7
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-08-04 16:46:30 -0400

    Add key for npcs

--- src/lib/localStorage/storageKeys.ts
+++ src/lib/localStorage/storageKeys.ts
@@ -1,5 +1,8 @@
+// Constant string values to be used as localStorage keys.
+// If you change a value here make sure to delete the old value from localStorage.
 export const storageKeys = {
   draftNpc: "draftNpc",
+  npcs: "npcs",
 } as const;
 
 export type StorageKey = (typeof storageKeys)[keyof typeof storageKeys];
