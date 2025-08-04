7e236277ca22e81ec05703dd4889c248b97d48bb
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-08-04 16:32:07 -0400

    Restructure error handling and add it to the useEffect

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,5 @@
 import type { StorageKey } from "@/lib/localStorage/storageKeys";
-import { useEffect, useState, type SetStateAction } from "react";
+import { useEffect, useRef, useState, type SetStateAction } from "react";
 import type { ZodType } from "zod";
 
 type UseLocalStorageProps<T> = {
@@ -13,17 +13,26 @@ export default function useLocalStorage<T>({
   schema,
   defaultValue,
 }: UseLocalStorageProps<T>): [T, React.Dispatch<SetStateAction<T>>] {
-  // Create react state initialized to local storage val or default val
+  // Create react state initialized to parsed local storage item else defaultValue
+  const touched = useRef(false);
   const [item, setItem] = useState(() => {
     try {
+      // Return defaultValue if no item in local storage
       const existingItemString = localStorage.getItem(key);
-      const parseResult = schema.safeParse(
-        JSON.parse(existingItemString || "{}"),
-      );
-      return parseResult.success ? parseResult.data : defaultValue;
+      if (!existingItemString) {
+        return defaultValue;
+      }
+      // Return parsed data if success else defaultValue
+      const parseResult = schema.safeParse(JSON.parse(existingItemString));
+      if (parseResult.success) {
+        return parseResult.data;
+      } else {
+        return defaultValue;
+      }
     } catch (error) {
+      // On error log error and return defaultValue
       console.error(
-        `There was an error while parsing ${key} in localStorage.`,
+        `Error while parsing ${key} in localStorage. Using default value.`,
         error,
       );
       return defaultValue;
@@ -32,8 +41,23 @@ export default function useLocalStorage<T>({
 
   // When this state is changed, update the local state to match it
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(item));
-  }, [item, key]);
+    try {
+      const parseResult = schema.safeParse(item);
+      if (parseResult.success) {
+        localStorage.setItem(key, JSON.stringify(item));
+        // Mark the state as touched
+        touched.current = true;
+        // If the state hasn't been touched yet then don't show error, as default values
+        // match the infered Zod schema but don't always match the expected data constraints
+      } else if (touched.current) {
+        console.error(
+          `Failed to set local storage item ${key}. Validation failed.`,
+        );
+      }
+    } catch (error) {
+      console.error(`Error while setting local storage item ${key}.`, error);
+    }
+  }, [item, key, schema]);
 
   return [item, setItem];
 }
