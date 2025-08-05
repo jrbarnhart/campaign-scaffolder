5d45a38ff27d1a892c176ffd00f9a36261178b74
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-08-05 15:03:08 -0400

    Add the local storage context provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import LocalStorageContextProvider from "./contexts/localStorage/LocalStorageContextProvider";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -22,7 +23,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
+      <LocalStorageContextProvider>
+        <RouterProvider router={router} />
+      </LocalStorageContextProvider>
+    </StrictMode>,
   );
 }
