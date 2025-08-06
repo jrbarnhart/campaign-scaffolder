fd94a50b98ce6d98379c0410f2a102d942195f23
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-08-06 19:27:25 -0400

    Add util for getting next int ID

--- src/lib/arrayUtils.ts
+++ src/lib/arrayUtils.ts
@@ -32,3 +32,26 @@ export function getManyFromArray<T>(arr: T[], count: number): T[] | undefined {
   const selected = shuffledArr.slice(0, count);
   return selected;
 }
+
+/**
+ * Gets the next available int id from an array of data that have the id property.
+ * This function trusts that the id values are positive integers.
+ * @param array An array of objects with property id: number.
+ * @returns The next available integer ID, starting from 1 if the array is empty.
+ */
+export function getNextId(array: { id: number }[]): number {
+  // If the array is empty the first id will be 0
+  if (array.length === 0) {
+    return 1;
+  }
+
+  // Else find the highest current id
+  let maxId = -Infinity;
+  for (const item of array) {
+    if (item.id > maxId) {
+      maxId = item.id;
+    }
+  }
+
+  return maxId + 1;
+}
